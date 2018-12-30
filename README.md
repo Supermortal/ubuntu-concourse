b0ed0005eb49567e149a84305471d8938ec1b31d
Supermortal <christopherbyronpink@yahoo.com>
2018-12-30 14:46:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Instructions
+
+## Create User And Assign Sudo
+
+* First run `sudo adduser concourse`
+* Add a password when prompted
+* Then run `sudo usermod -aG sudo concourse`
+* Finally run `su - concourse` to switch to the new user
+
+## Add Environment Variables To /etc/environment
+
+* Check .env.sample for variables that need to be set
+  * It can help to have a .env file with your variables in it
+  * This file is already ignored by the .gitignore
+* Run `sudo vi /etc/environment` or `sudo nano /etc/environment` (or use the text editor of your choice)
+* Copy your variables from your .env file (or manually edit them) into the environment file
+
+## Clone This Repository
+
+* Make sure you're in the "~" directory (or whatever directory you want to keep the files)
+  * `cd ~` to get to the directory
+* Run `git clone https://github.com/Supermortal/ubuntu-concourse.git`
+* Run `cd ubuntu-concourse`
+
+## Run init.sh
+
+* Run `./init.sh`
+* This command should add all the dependecies your need, including the fly CLI
+* Your Concourse instance should now be available at your desired host at port 80
---
+++ init.sh
@@ -0,0 +1,41 @@
+#DOCKER:
+
+sudo apt-get -y update
+
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+sudo apt -y install docker-compose
+
+sudo docker build -t reverseproxy .
+
+#CONCOURSE:
+
+./generate-keys.sh
+sudo docker-compose up -d
+
+#FLY:
+
+echo "Waiting on Concourse server"
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://127.0.0.1:80/api/v1/cli?arch=amd64&platform=linux)" != "400" ]]; do sleep 1; printf "%c" "."; done'
+curl "http://127.0.0.1:80/api/v1/cli?arch=amd64&platform=linux" --output fly
+
+sudo mkdir -p /usr/local/bin
+sudo cp fly /usr/local/bin
+sudo chmod 0755 /usr/local/bin/fly
+
+fly -t main login -c http://127.0.0.1:80 -u "$CONCOURSE_MAIN_TEAM_LOCAL_USER" -p "$CONCOURSE_LOCAL_USER_PASSWORD"
