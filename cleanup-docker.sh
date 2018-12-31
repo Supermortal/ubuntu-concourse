bff181f97edca9f2fb491466a6ee1f4762a8b46f
Supermortal <christopherbyronpink@yahoo.com>
2018-12-31 00:29:25 -0700

    Add sudo to remove

--- cleanup-docker.sh
+++ cleanup-docker.sh
@@ -9,4 +9,4 @@ sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r su
 # remove unused volumes:
 sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
   sudo docker ps -aq | xargs sudo docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
-) | xargs -r rm -fr
+) | xargs -r sudo rm -fr
