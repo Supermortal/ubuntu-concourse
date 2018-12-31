df017f61a4e1454ec36707090573b9bf3027610a
Supermortal <christopherbyronpink@yahoo.com>
2018-12-31 00:28:53 -0700

    Add sudo to find

--- cleanup-docker.sh
+++ cleanup-docker.sh
@@ -7,6 +7,6 @@ sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r sudo d
 sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r sudo docker rmi
 
 # remove unused volumes:
-find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+sudo find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
   sudo docker ps -aq | xargs sudo docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
 ) | xargs -r rm -fr
