82a2b2b08f05bdd6659fc63460d937a114f89e55
Supermortal <christopherbyronpink@yahoo.com>
2018-12-31 00:28:20 -0700

    Add clean up docker script

---
+++ cleanup-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# remove exited containers:
+sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r sudo docker rm -v
+    
+# remove unused images:
+sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r sudo docker rmi
+
+# remove unused volumes:
+find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+  sudo docker ps -aq | xargs sudo docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
+) | xargs -r rm -fr
